463ebf250f4de62afcb0dd63afae540e8e9c5b10
AhmedTawfiqM <ahmedadelmm26@yahoo.com>
2020-04-21 02:12:02 +0200

    ""

---
+++ android/app/src/main/java/com/atdev/secondapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.atdev.secondapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: MyHome(),
+    ));
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('TifaApp'),
+        backgroundColor: Colors.grey[600],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 10),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 10,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/profile.png'),
+                    radius: 40.0,
+                  ),
+                ),
+                Expanded(
+                  flex: 10,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/profile.png'),
+                    radius: 40.0,
+                  ),
+                ),
+                Expanded(
+                  flex: 10,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/profile.png'),
+                    radius: 40.0,
+                  ),
+                ),
+                Expanded(
+                  flex: 10,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/profile.png'),
+                    radius: 40.0,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/profile.png'),
+                radius: 40.0,
+              ),
+            ),
+            SizedBox(height: 10),
+            Divider(height: 10,color: Colors.amber),
+            SizedBox(height: 10),
+            Text(
+              'Ahmed Tawfiq',
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'DancingScript',
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'DancingScript',
+                fontWeight: FontWeight.bold,
+                color: Colors.amber[600],
+                letterSpacing: 1,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Currently Flutter Level',
+              style: TextStyle(
+                fontSize: 14,
+                fontFamily: 'DancingScript',
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[600],
+                letterSpacing: 1,
+              ),
+            ),
+            SizedBox(height: 1),
+            Text(
+              '8',
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'DancingScript',
+                fontWeight: FontWeight.bold,
+                color: Colors.amber[600],
+                letterSpacing: 1,
+              ),
+            ),
+            SizedBox(height: 1),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[600],
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    'ahmedadelmm26@gmail.com',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'DancingScript',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[600],
+                      letterSpacing: 1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            RaisedButton(
+              padding: EdgeInsets.all(10),
+              color: Colors.black,
+              child: Text(
+                'Contact',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'DancingScript',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber[600],
+                  letterSpacing: 1,
+                ),
+              ),
+              onPressed: (){},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
